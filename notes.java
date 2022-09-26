50507509203c010a87edd006e2a410ce05c1231a
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-09-26 06:47:02 +0300

    remembering java

---
+++ notes.java
@@ -0,0 +1,23 @@
+class notes {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+
+        int my_int = -5;
+        System.out.println("my_int = " + my_int);
+
+        long my_long = 5;
+        System.out.println("my_long = " + my_long);
+
+        double my_double = 5.0;
+        System.out.println("my_double = " + my_double);
+
+        float my_float = (float) 5.0;
+        float my_float2 = 5.0f;
+        System.out.println("my_float = " + my_float + " " + my_float2);
+
+        char my_char = 'a';
+        char my_char2 = '\u00aa';
+        System.out.println("my_char = " + my_char + " " + my_char2);
+
+    }
+}
